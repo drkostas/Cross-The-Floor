4230e0265a510b5c7cc737ac4ae6374e832ea89d
Konstantinos Georgiou <georgiou.kostas94@gmail.com>
2019-12-26 04:15:55 +0200

    Created requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+setup(
+    name='parliament_members_sankey',
+    version='0.1',
+    py_modules=['main'],
+    # packages=['crawler', 'visualizer', 'configuration', 'pandas_manager'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/drkostas',
+    license='',
+    author='drkostas',
+    author_email='georgiou.kostas94@gmail.com',
+    description='Package for creating sankey diagrams for political',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'parliament_members_sankey=main:main'
+        ]
+    },
+)
